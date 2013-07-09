99050b82b1d262cea7e7587a0deebe1bdf1209da
Chris Lucas <chris@chrisjlucas.com>
2013-07-08 19:51:01 -0700

    Initial Commit

---
+++ CJUtils/CJUtils.h
@@ -0,0 +1,13 @@
+//
+//  CJUtils.h
+//  CJUtils
+//
+//  Created by Chris Lucas on 7/8/13.
+//  Copyright (c) 2013 Chris Lucas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CJUtils : NSObject
+
+@end
