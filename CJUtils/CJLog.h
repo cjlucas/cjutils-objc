fb2bba5a362bfab679f9bc14474497365817e848
Chris Lucas <chris@chrisjlucas.com>
2013-07-10 14:08:12 -0700

    [api] added basic authentication support

---
+++ CJUtils/CJLog.h
@@ -0,0 +1,24 @@
+//
+//  CJLog.h
+//  CJUtils
+//
+//  Created by Chris Lucas on 7/9/13.
+//  Copyright (c) 2013 Chris Lucas. All rights reserved.
+//
+
+#include "string.h"
+
+#ifndef CJUtils_CJLog_h
+#define CJUtils_CJLog_h
+
+#ifdef DEBUG
+
+#define BASEFILENAME (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+#define CJLog(fmt, ...) do { NSLog(@"[%s:%d] %s " fmt, BASEFILENAME, __LINE__, __PRETTY_FUNCTION__, @"" __VA_ARGS__); } while(0)
+
+#else
+#define CJLog(...) do {} while(0)
+
+#endif
+
+#endif
